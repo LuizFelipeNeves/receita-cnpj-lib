cb03a7f1f8d497a8d116e20fd25fd18e8118ef0c
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2023-07-05 11:51:35 -0300

    Fix encoding

--- R/scrape.R
+++ R/scrape.R
@@ -1,9 +1,13 @@
 fix_html <- function (x, arq_html) {
-  text <- httr::content(x, "text", 'text/html', 'latin1') %>%
+  encoding <- 'latin1'
+  text <- httr::content(x, "text", 'text/html', encoding) %>%
     stringr::str_replace(
     pattern = "images/brasao2.gif", 
     replace = "http://www.receita.fazenda.gov.br/PessoaJuridica/CNPJ/cnpjreva/images/brasao2.gif")
-  writeLines(text, arq_html)
+
+  con <- file(arq_html, open = "w+", encoding = encoding)
+  writeLines(text, con = con)
+  close(con)
 }
 
 scrape_cnpj <- function(x) {
@@ -14,7 +18,7 @@ scrape_cnpj <- function(x) {
   txts <- txts[txts != '']
   dados <- txts %>%
     stringr::str_split_fixed(' \n', 2) %>%
-    tibble::as_tibble() %>%
+    tibble::as_tibble(.name_repair = 'unique') %>%
     purrr::set_names(c('key', 'value'))
   return(dados)
 }
@@ -25,7 +29,7 @@ scrape_qsa <- function(x) {
     rvest::html_nodes('fieldset') %>%
     arrumar_node() %>%
     stringr::str_split_fixed(' \n', 4) %>%
-    tibble::as_tibble() %>%
+    tibble::as_tibble(.name_repair = 'unique') %>%
     purrr::set_names(c('nm_key', 'nm', 'qual_key', 'qualif')) %>%
     dplyr::select(nm, qualif)
   xp <- '//div[@id="principal"]//table[2]//table//tr'
@@ -33,7 +37,7 @@ scrape_qsa <- function(x) {
     rvest::html_nodes(xpath = xp) %>%
     arrumar_node() %>%
     stringr::str_split_fixed(' \n', 2) %>%
-    tibble::as_tibble() %>%
+    tibble::as_tibble(.name_repair = 'unique') %>%
     purrr::set_names(c('key', 'value')) %>%
     dplyr::mutate(key = stringr::str_replace(key, ':$', ''))
   return(list(infos_basicas = kv, qsa = fs))
